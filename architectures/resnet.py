522e724517e5cd95a351702b1a1608e7901bd895
Yilong Song <songyilong18@gmail.com>
2024-10-02 20:13:16 -0400

    functioning resnet

--- architectures/resnet.py
+++ architectures/resnet.py
@@ -3,20 +3,36 @@ import torch.nn as nn
 import torchvision.models as models
 
 class ResNet18(nn.Module):
-    def __init__(self, output_size=7):
+    def __init__(self, output_size=7, mlp_hidden_size=512, num_mlp_layers=5):
         super(ResNet18, self).__init__()
         
         self.resnet = models.resnet18(pretrained=False)
         
-        self.conv_part = nn.Sequential(*list(self.resnet.children())[:-1])  # All layers except the last FC
+        self.conv_part = nn.Sequential(*list(self.resnet.children())[:-1])
         
-        self.mlp_part = nn.Linear(in_features=self.resnet.fc.in_features, out_features=output_size)
+        mlp_layers = []
+        input_size = self.resnet.fc.in_features  # Input size for the MLP, which is 512 in ResNet18
+
+        for _ in range(num_mlp_layers - 1):
+            mlp_layers.append(nn.Linear(input_size, mlp_hidden_size))
+            mlp_layers.append(nn.ReLU())
+            mlp_layers.append(nn.BatchNorm1d(mlp_hidden_size))
+            mlp_layers.append(nn.Dropout(0.5))
+            input_size = mlp_hidden_size
+            
+        # Constrict to 49 perceptrons
+        mlp_layers.append(nn.Linear(mlp_hidden_size, 49))
+        
+        # Final layer that outputs the action vector
+        mlp_layers.append(nn.Linear(49, output_size))
+        
+        # Define MLP as a Sequential model
+        self.mlp_part = nn.Sequential(*mlp_layers)
     
     def forward(self, x):
         conv_features = self.conv_part(x)
-        
+        # Flatten the convolutional output to feed into the MLP
         conv_features = conv_features.view(conv_features.size(0), -1)
-        
         output = self.mlp_part(conv_features)
         
         return output
